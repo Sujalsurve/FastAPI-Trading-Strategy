a96dd4bbce79c59ae31a8b97931cabe27ecdecac
Sujal Surve <118345158+Sujalsurve@users.noreply.github.com>
2025-02-10 17:03:48 +0530

    Add files via upload

---
+++ fastapi_trading_strategy/sql file/query.sql
@@ -0,0 +1,9 @@
+CREATE TABLE stock_data (
+    id SERIAL PRIMARY KEY,
+    datetime TIMESTAMP NOT NULL,
+    open DECIMAL(10,2) NOT NULL,
+    high DECIMAL(10,2) NOT NULL,
+    low DECIMAL(10,2) NOT NULL,
+    close DECIMAL(10,2) NOT NULL,
+    volume INTEGER NOT NULL
+);
